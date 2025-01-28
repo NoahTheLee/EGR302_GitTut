7f86265407293fc7ffc984e46c5ae9a2de40ac44
Noah Lee <noahjefferson.lee@calbaptist.edu>
2025-01-27 19:39:59 -0800

    New branch created

---
+++ BranchFileA.java
@@ -0,0 +1,5 @@
+public class BranchFileA {
+    public static void main(String[] args) {
+        System.out.println("Hello from BranchFileA!");
+    }
+}
---
+++ BranchFileB.java
@@ -0,0 +1,5 @@
+public class BranchFileB {
+    public static void main(String[] args) {
+        System.out.println("Hello from BranchFileB!");
+    }
+}
