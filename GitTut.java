a8619171a882cb1500ef28759ec25b6ffe2f83eb
Noah Lee <noahjefferson.lee@calbaptist.edu>
2025-01-27 19:37:03 -0800

    Moving portions of a file over

--- GitTut.java
+++ GitTut.java
@@ -2,5 +2,7 @@ public class GitTut {
     public static void main(String[] args) {
         System.out.println("Hello World! this is a test file!");
         System.out.println("Noah Wuz Here");
+        System.out.println("This is a new line ");
+        System.out.println("That's not a new line, this is!");
     }
 }
