1482528906f20fc8b5601304bf5a7dbab5bfed5e
Noah Lee <noahjefferson.lee@calbaptist.edu>
2025-01-27 19:48:02 -0800

    finished data verification

--- BranchFileB.java
+++
@@ -1,5 +0,0 @@
-public class BranchFileB {
-    public static void main(String[] args) {
-        System.out.println("Hello from BranchFileB!");
-    }
-}
---
+++ secondbranch.java
@@ -0,0 +1,7 @@
+public class secondbranch {
+    public static void main(String[] args) {
+        System.out.println("I'm doing data verification!");
+        System.out.println("Sure is hard doing this verification...");
+        System.out.println("Changes completed!");
+    }
+}
